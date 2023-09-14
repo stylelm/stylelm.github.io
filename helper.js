8d2f2f7f819d3c2c0f26fb301beb78bcf10ebb8c
cyanbx <obama1205@yeah.net>
2023-09-14 16:47:21 +0800

    adding s2ut in vc

--- helper.js
+++ helper.js
@@ -41,7 +41,7 @@ function generateFrEn(tableId) {
 
 function generateVC(tableId) {
   let table = document.getElementById(tableId);
-  let ext = ['_source.wav', '_cvsst.wav', '_nansy.wav', '_ppgvc.wav', '_yourtts.wav', '_stylelm.wav'];
+  let ext = ['_source.wav', '_s2ut.wav', '_ppgvc.wav', '_nansy.wav', '_yourtts.wav', '_stylelm.wav', '_cvsst.wav'];
 
   for (var i = 0; i < 4; i++) {
     generateExampleRow(table.rows[1 + i], 'data/vc/es_'  + i, ext, 0);

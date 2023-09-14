87872fa5b99f82ca0a0460ac7a5c9aca98bee6f3
cyanbx <obama1205@yeah.net>
2023-09-14 16:21:16 +0800

    add samples

--- helper.js
+++ helper.js
@@ -9,7 +9,6 @@ function generateExampleRow(table_row, base_path, filename_ext, col_offset) {
   for (var i = 0; i < filename_ext.length; i++) {
     let p = base_path + filename_ext[i];
     let cell = table_row.cells[col_offset + i];
-    console.log(table_row.cells.length)
     if (p.endsWith('txt')) {
       var req = new XMLHttpRequest();
       req.open("GET", p, false);
@@ -22,84 +21,37 @@ function generateExampleRow(table_row, base_path, filename_ext, col_offset) {
 }
 
 
-function generateTextToSpeech(tableId) {
+function generateEsEn(tableId) {
   let table = document.getElementById(tableId);
-  let ext = ['_text.txt', '_prompt.wav', '_metastylespeech.wav', '_yourtts.wav', '_generspeech.wav', '_ours.wav'];
+  let ext = ['_source.wav', '_cvsst.wav', '_s2ut.wav', '_ms.wav', '_stylelm.wav'];
 
-  for (var i = 0; i < 5; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/tts/' + i, ext, 0);
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i * 3], 'data/es_en/es_'  + i, ext, 1);
   }
 }
 
-function generateSingingVoiceSynthesis(tableId) {
+function generateFrEn(tableId) {
   let table = document.getElementById(tableId);
-  let ext = ['_text.txt', '_gt.wav', '_prompt.wav', '_fftsinger.wav', '_diffsinger.wav', '_ours.wav'];
+  let ext = ['_source.wav', '_cvsst.wav', '_s2ut.wav', '_ms.wav', '_stylelm.wav'];
 
-  for (var i = 0; i < 5; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/m4singer/' + i, ext, 0);
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i * 3], 'data/fr_en/fr_'  + i, ext, 1);
   }
 }
 
-function generateVoiceConversion(tableId) {
+function generateVC(tableId) {
   let table = document.getElementById(tableId);
-  let ext = ['_source.wav', '_prompt.wav', '_nansy.wav', '_ppg-vc.wav', '_ours.wav'];
+  let ext = ['_source.wav', '_cvsst.wav', '_nansy.wav', '_ppgvc.wav', '_yourtts.wav', '_stylelm.wav'];
 
-  for (var i = 0; i < 5; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/vc/' + i, ext, 0);
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/vc/es_'  + i, ext, 0);
+  }
+
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[5 + i], 'data/vc/fr_'  + i, ext, 0);
   }
 }
 
-function generateCrossLingual(tableId) {
-  let table = document.getElementById(tableId);
-  let ext = ['_ref.wav', '_prompt.wav', '_syn.wav'];
-
-  for (var i = 0; i < 3; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/cross_new/' + i, ext, 0);
-  }
-}
-
-function generateNoise(tableId) {
-  let table = document.getElementById(tableId);
-  let ext = ['_ref.wav', '_prompt.wav', '_syn.wav'];
-
-  for (var i = 0; i < 3; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/noise_new/' + i, ext, 0);
-  }
-}
-
-function generateEmotion(tableId) {
-  let table = document.getElementById(tableId);
-  let ext = ['_ref.wav', '_prompt.wav', '_syn.wav'];
-
-  for (var i = 0; i < 3; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/emotion_new/' + i, ext, 1);
-  }
-}
-
-function generateValle(tableId) {
-  let table = document.getElementById(tableId);
-  let ext = ['_text.txt', '_prompt.wav', '_valle.wav','_ours.wav'];
-
-  for (var i = 0; i < 3; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/valle/' + i, ext, 0);
-  }
-}
-
-function generateSpearTTS(tableId) {
-  let table = document.getElementById(tableId);
-  let ext = ['_text.txt', '_prompt.wav', '_speartts.wav','_ours.wav'];
-
-  for (var i = 0; i < 2; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/speartts/' + i, ext, 0);
-  }
-}
-
-
-generateTextToSpeech('text-to-speech-table')
-generateVoiceConversion('voice-conversion-table')
-generateSingingVoiceSynthesis('singing-voice-synthesis-table')
-generateCrossLingual('crosslingual-table')
-generateNoise('noise-table')
-generateEmotion('emotion-table')
-generateValle('valle-table')
-generateSpearTTS('speartts-table')
+generateEsEn('trans_es_table')
+generateFrEn('trans_fr_table')
+generateVC('vc_table')
